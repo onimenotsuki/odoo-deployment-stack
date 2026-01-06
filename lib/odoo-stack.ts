be90123c3f8270cdc61a2f1f2628dab1cd00f5cc
Edgar Talledos <onimenotsuki@gmail.com>
2026-01-06 17:57:10 -0600

    chore: renamed stack

--- lib/odoo-stack.ts
+++ lib/odoo-stack.ts
@@ -3,7 +3,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
-export class OdooDeploymentStack extends cdk.Stack {
+export class OdooStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
