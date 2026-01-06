5fbb0433a4b64847c57cade64ac3af00a58df9c2
Edgar Talledos <onimenotsuki@gmail.com>
2026-01-06 12:48:53 -0600

    Initial commit

---
+++ test/odoo-deployment-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OdooDeploymentStack from '../lib/odoo-deployment-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/odoo-deployment-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OdooDeploymentStack.OdooDeploymentStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
